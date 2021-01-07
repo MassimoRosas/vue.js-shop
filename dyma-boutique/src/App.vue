d8ad56cbfc62e6b9ba67b02622459112ab606fb4
Massimo <massimo.rosas279@gmail.com>
2021-01-07 10:34:36 +0100

    Added header

--- dyma-boutique/src/App.vue
+++ dyma-boutique/src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div id="app">
-    
+    <TheHeader></TheHeader>
   </div>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader';
 
 export default {
   name: 'App',
   components: {
-    
+    TheHeader
   }
 }
 </script>

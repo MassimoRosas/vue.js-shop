bc42d28666a848b71bc57e876d89647828ab1dc7
Massimo <massimo.rosas279@gmail.com>
2021-01-12 16:16:22 +0100

    handled form reactivity

--- dyma-boutique/src/components/features/Admin/AdminProductForm.vue
+++ dyma-boutique/src/components/features/Admin/AdminProductForm.vue
@@ -1,30 +1,69 @@
 <template>
-  <form class="d-flex flex-column">
+  <form @submit="trySubmit" class="d-flex flex-column">
     <h4>Ajouter un produit :</h4>
     <hr class="w-100">
     <div class="form-group p-2">
       <label>Image</label>
-      <input type="text" class="form-control" />
+      <input v-model="form.img" type="text" class="form-control" />
     </div>
     <div class="form-group p-2">
       <label>Titre</label>
-      <input type="text" class="form-control" />
+      <input v-model="form.title" type="text" class="form-control" />
     </div>
     <div class="form-group p-2">
       <label>Description</label>
-      <textarea class="form-control"></textarea>
+      <textarea v-model="form.description" class="form-control"></textarea>
     </div>
     <div class="form-group p-2">
       <label>Prix</label>
-      <input type="number" class="form-control" />
+      <input v-model.number="form.price" type="number" class="form-control" />
     </div>
+    <ul v-if="errors.length">
+      <li class="text-danger" v-for="error in errors" :key="error">{{ error }}</li>
+    </ul>
     <button class="btn btn-primary m-2">Ajouter</button>
   </form>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      form: {
+        img: '',
+        title: '',
+        description: '',
+        price: ''
+      },
+      errors: []
+    }
+  },
+  methods: {
+    trySubmit(e) {
+      e.preventDefault();
+      if(this.formIsValid()) {
+        console.log(this.form);
+      }
+    },
+    formIsValid() {
+      this.errors = [];
+
+      if(!this.form.img) {
+        this.errors.push('img required');
+      }
+      if(!this.form.title) {
+        this.errors.push('title required');
+      }
+      if(!this.form.description) {
+        this.errors.push('description required');
+      }
+      if(!this.form.price) {
+        this.errors.push('price required');
+      }
+
+      return this.errors.length ? false : true;
+    }
+  }
 }
 </script>
 

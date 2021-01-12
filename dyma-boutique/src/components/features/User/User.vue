3b12646e24183cfde432c363c17df6da4ef4d808
Massimo <massimo.rosas279@gmail.com>
2021-01-12 14:58:51 +0100

    New architecture

---
+++ dyma-boutique/src/components/features/User/User.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="d-flex flex-row">
+    <Shop class="w-75"></Shop>
+    <Cart class="w-25"></Cart>
+  </div>
+</template>
+
+<script>
+
+import Shop from './Shop/Shop';
+import Cart from './Cart/Cart';
+
+export default {
+  components: {
+    Shop,
+    Cart
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
